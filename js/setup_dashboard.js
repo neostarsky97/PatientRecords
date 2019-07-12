b5881f7c396a3a877a5d16ce4823902f290120b6
Gururaj S Acharya <gururaj.s.acharya@philips.com>
2019-07-12 15:23:28 +0530

    fixed setup_dashboard to display patient data

--- js/setup_dashboard.js
+++ js/setup_dashboard.js
@@ -9,16 +9,37 @@
         var row = table.insertRow(0);
         var cell = row.insertCell(0);
         cell.innerHTML = actualPatientObject.getMrn();
-        row.addEventListener('click', function(){
+        var callBack = function(){
             var display = globalRef.document.getElementById('patient_info');
             var data = '';
-            data += 'Name: ' + actualPatientObject.getName() + '<br>';
-            data += 'Age: ' + actualPatientObject.getAge() + '<br>';
-            data += 'Gender: ' + actualPatientObject.getGender() + '<br>';            
+            data += 'Name: ' + this.getName() + '<br>';
+            data += 'Age: ' + this.getAge() + '<br>';
+            data += 'Gender: ' + this.getGender() + '<br>';            
             display.innerHTML = data;
-        });
+        }
+        callBack = callBack.bind(actualPatientObject);
+        row.addEventListener('click', callBack);
+
+        var parent = globalRef.document.getElementById('patient_show_record_button');
+        var button = globalRef.document.createElement("Show Records");
+        var recordCallBack = function () {
+            var record_table = ;
+            
+            var patientRecords = this.getReports();
+            for (var record_index = 0; record_index < patientRecords.length; record_index++) {
+                var record_row = record_table.insertRow(record_index);
+
+                var drnCell = record_row
+            }
+        }
+
+        recordCallBack = recordCallBack.bind(actualPatientObject);
+        button.addEventListener('click', recordCallBack);
+        parent.appendChild(button);
     }
 
     globalRef.document.getElementById('showRecord').addEventListener('click', function() {
+        var table = globalRef.document.getElementById('rtable');
+        
     });
 })(window);
