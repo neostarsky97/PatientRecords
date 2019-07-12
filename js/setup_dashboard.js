6e3d48b7a028a3c88a652c658bed3c073a50d73a
Gururaj S Acharya <gururaj.s.acharya@philips.com>
2019-07-12 16:39:12 +0530

    Hello

--- js/setup_dashboard.js
+++ js/setup_dashboard.js
@@ -20,26 +20,36 @@
         callBack = callBack.bind(actualPatientObject);
         row.addEventListener('click', callBack);
 
-        var parent = globalRef.document.getElementById('patient_show_record_button');
-        var button = globalRef.document.createElement("Show Records");
+        var parent = globalRef.document.getElementById('patient_display');
+        var button = globalRef.document.createElement('button');
+
         var recordCallBack = function () {
-            var record_table = ;
+            var record_table = globalRef.document.getElementById('rtable_tbody');
             
-            var patientRecords = this.getReports();
-            for (var record_index = 0; record_index < patientRecords.length; record_index++) {
+            while(record_table.hasChildNodes()) {
+                record_table.removeChild(record_table.firstChild);
+            }
+
+            for (var record_index = 0; record_index < this.length; record_index++) {
                 var record_row = record_table.insertRow(record_index);
 
-                var drnCell = record_row
+                console.log(this);
+                var drnCell = record_row.insertCell(0);
+                drnCell.innerHTML += this[record_index]._drn;
+            
+                var dateCell = record_row.insertCell(1);
+                dateCell.innerHTML += this[record_index]._date;
+
+                var doctorCell = record_row.insertCell(2);
+                doctorCell.innerHTML += this[record_index]._doctor;
+
+                var messageCell = record_row.insertCell(3);
+                messageCell.innerHTML += this[record_index]._message;
             }
         }
 
-        recordCallBack = recordCallBack.bind(actualPatientObject);
+        recordCallBack = recordCallBack.bind(actualPatientObject._reports);
         button.addEventListener('click', recordCallBack);
         parent.appendChild(button);
     }
-
-    globalRef.document.getElementById('showRecord').addEventListener('click', function() {
-        var table = globalRef.document.getElementById('rtable');
-        
-    });
 })(window);
